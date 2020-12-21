980b8c56c2681b18f90960532713b39cd18f76ff
yashdilipjoshi <yashdilipjoshi@gmail.com>
2020-12-21 15:16:45 +0530

    commit

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -12,11 +12,15 @@ public class Calc {
     public static int mul(int num1,int num2) {
     	return num1*num2;
     }
+    public static int div(int num1,int num2) {
+    	return num1/num2;
+    }
 	public static void main(String[] args) {
 		
    System.out.println(add(10,20));
    System.out.println(sub(30,20));
    System.out.println(mul(30,20));
+   System.out.println(div(30,3));
 	}
 
 }
