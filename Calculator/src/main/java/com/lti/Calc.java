f45ee3d36853b6186f16ad3f05feae803c476e9e
yashdilipjoshi <yashdilipjoshi@gmail.com>
2020-12-21 14:58:46 +0530

    new commit

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -4,9 +4,14 @@ public class Calc {
     public static int add(int num1,int num2) {
     	return num1+num2;
     }
+    
+    public static int sub(int num1,int num2) {
+    	return num1-num2;
+    }
 	public static void main(String[] args) {
 		
    System.out.println(add(10,20));
+   System.out.println(sub(30,20));
 	}
 
 }
