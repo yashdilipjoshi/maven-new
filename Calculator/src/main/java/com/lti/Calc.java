1c3c3980fe9b242c3324cb5a45354808f877e75b
yashdilipjoshi <yashdilipjoshi@gmail.com>
2020-12-21 15:09:32 +0530

    new commit

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -8,10 +8,15 @@ public class Calc {
     public static int sub(int num1,int num2) {
     	return num1-num2;
     }
+    
+    public static int mul(int num1,int num2) {
+    	return num1*num2;
+    }
 	public static void main(String[] args) {
 		
    System.out.println(add(10,20));
    System.out.println(sub(30,20));
+   System.out.println(mul(30,20));
 	}
 
 }
